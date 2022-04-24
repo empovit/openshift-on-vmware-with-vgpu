76c64efb2be4331420c2907d828b294cb87c5dd7
Vitaliy Emporopulo <vemporop@redhat.com>
2022-04-24 12:35:25 +0300

    Add vCenter installation scripts, refactor S3

---
+++ templates/install_vgpu_vib.py
@@ -0,0 +1,20 @@
+from pyVmomi import vim
+from pyVim import connect
+from vars import (
+    private_subnets,
+    public_subnets,
+    public_cidrs,
+    esx_passwords,
+    vcenter_username,
+    sso_password,
+    dc_name,
+    vcenter_cluster_name,
+    vcenter_network,
+    domain_name,
+)
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
