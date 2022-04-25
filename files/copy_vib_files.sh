5b4f5eee79145a1418d4979ee4d2375d6bea91b2
Vitaliy Emporopulo <vemporop@redhat.com>
2022-04-25 13:37:07 +0300

    Implement vGPU installation on cluster

--- files/copy_vib_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ESXI_HOST="$1"
-VIB_FILENAME="$2"
-
-scp "$HOME/bootstrap/${VIB_FILENAME}" "root@${ESXI_HOST}:/vmfs/volumes/datastore1/${VIB_FILENAME}"
-scp "$HOME/bootstrap/install_vib.sh" "root@${ESXI_HOST}:/vmfs/volumes/datastore1/install_vib.sh"
